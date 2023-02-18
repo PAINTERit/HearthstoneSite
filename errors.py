8803f0138dba319dab64085785b2367360a287a8
artem <artemv2001@mail.ru>
2023-02-18 19:13:35 +0300

    Добавлены новые ошибки и type hint.

--- errors.py
+++ errors.py
@@ -4,10 +4,30 @@ from app import app
 
 
 @app.errorhandler(404)
-def error404(status):
+def error404(status) -> str:
     """
     Функция обрабатывает ошибку HTTP 404.
     :param status: int(Код ошибки)
     :return: error404.html (Шаблон страницы ошибки)
     """
     return render_template('errors/error_404.html')
+
+
+@app.errorhandler(403)
+def error403(status) -> str:
+    """
+    Функция обрабатывает ошибку HTTP 403.
+    :param status: int(Код ошибки)
+    :return: error403.html (Шаблон страницы ошибки)
+    """
+    return render_template('errors/error_403.html')
+
+
+@app.errorhandler(500)
+def error500(status) -> str:
+    """
+    Функция обрабатывает ошибку HTTP 500.
+    :param status: int(Код ошибки)
+    :return: error500.html (Шаблон страницы ошибки)
+    """
+    return render_template('errors/error_500.html')
