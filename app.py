becedb7cfaf069e7fa4cdb7dede0ab5e72a15dd6
artem <artemv2001@mail.ru>
2023-02-24 16:58:25 +0300

    Форматирование с помощью black и isort. Косметические изменения. Добавлен секретный ключ с помощью uuid.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import uuid
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_toastr import Toastr
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_toastr import Toastr
 
 app = Flask(__name__)
 app.debug = True
@@ -11,15 +13,12 @@ UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
-
-app.config['SECRET_KEY'] = 'jwdjdwkjdwkdwkdw'
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = uuid.uuid4()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['TOASTR_SHOW_METHOD'] = 'show'
+app.config['TOASTR_TIMEOUT'] = 5000
+app.config['DEBUG'] = True
 toolbar = DebugToolbarExtension(app)
-
 db = SQLAlchemy(app)
 manager = LoginManager(app)
 toastr = Toastr(app)
-app.config['TOASTR_SHOW_METHOD'] = 'show'
-app.config['TOASTR_TIMEOUT'] = 5000
-
